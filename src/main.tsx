38610ffddbc5b9584743cfdbf5e59e14d2e86f26
claralawrence00 <jameshoffmanbs@gmail.com>
2025-12-02 09:44:29 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import "particles.js";
-declare const particlesJS: any;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
-
-particlesJS("particles-js", {
-  particles: {
-    number: { value: 50, density: { enable: true, value_area: 1000 } },
-    color: { value: "#ffffff" },
-    shape: { type: "circle" },
-    opacity: { value: 0.3, random: true },
-    size: { value: 4, random: true },
-    line_linked: { enable: false },
-    move: { enable: true, speed: 1.2, random: true, out_mode: "out" }
-  },
-  interactivity: {
-    events: { onhover: { enable: false }, onclick: { enable: false } }
-  },
-  retina_detect: true
-});
 
